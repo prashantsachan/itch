e7cb8ec13fd0593feebb71d5579b0bbf34db6bc3
Sachan <pssachan@amazon.com>
2017-11-30 13:47:09 +0530

    [coursera-crypto/consensus]: wait for few rounds to detect malicious nodes
    * transmit only the txns given till few rounds
    * if a node chagnes its output till those rounds, it malicious
    * in all future rounds send everything that a hones node sends

--- coursera-cryptocurrency/02Consensus/assignment2starterCode/CompliantNode.java
+++ coursera-cryptocurrency/02Consensus/assignment2starterCode/CompliantNode.java
@@ -10,31 +10,24 @@ import java.util.function.Function;
 
 /* CompliantNode refers to a node that follows the rules (not malicious)*/
 public class CompliantNode implements Node {
-	private static final double contFactor = 0.2;
+	private static final double holdRoundFraction = 0.4;
+	private final int nHold;
 	private Set<Transaction> receivedTxns;
-	private HashMap<Transaction, Set<Integer>> txnSenders;
-	private double graphFactor;
-	private int acceptThreshold;
 	private final int nRounds;
 	private int doneRounds;
-	private HashMap<Integer, Map<Transaction, Integer>> contReports;
-	
+	private Map<Integer, Set<Transaction>>lastTransmitted;
+	private Set<Integer> malNodes;
 	
     public CompliantNode(double p_graph, double p_malicious, double p_txDistribution, int numRounds) {
     		receivedTxns = new HashSet<>();
-    		txnSenders = new HashMap<>();
-    		graphFactor = 1/(1+10*p_malicious) ; // varies from [0.6 - 1.0] * (0.0- 1.0) =>  (0.0 to 1.0]
     		nRounds = numRounds;
     		doneRounds= 0;
-    		contReports = new HashMap<>();
+    		lastTransmitted = new HashMap<>();
+    		malNodes = new HashSet<>();
+    		nHold = (int) Math.round(holdRoundFraction*nRounds);
     }
 
     public void setFollowees(boolean[] followees) {
-    		int count = 0;
-    		for(boolean f : followees)
-    			count+= f?1:0;
-    		acceptThreshold = (int) Math.round(graphFactor * count);
-    		System.out.printf("graphFactor: %3f, count: %d, threshold: %d nodes %n", graphFactor,count, acceptThreshold);
     }
 
     public void setPendingTransaction(Set<Transaction> pendingTransactions) {
@@ -42,63 +35,45 @@ public class CompliantNode implements Node {
     }
 
     public Set<Transaction> sendToFollowers() {
-    		if(doneRounds < nRounds)
-    			return new HashSet<>(receivedTxns);
-    		else {
-//    			System.out.println("sending final set of txns");
-    			HashSet<Transaction> finalSet = new HashSet<>();
-    			for(Transaction t: txnSenders.keySet()) {
-//    				if(txnSenders.get(t).size()>= acceptThreshold)
-    					finalSet.add(t);
-    			}
-    			return finalSet;
-    		}
-    }
+		return new HashSet<>(receivedTxns);
+   }
 
     public void receiveFromFollowees(Set<Candidate> candidates) {
     		doneRounds++;
-    		// every followee's reports in this round
-    		HashMap<Integer, Set<Transaction>> curReports = new HashMap<>();
-    		for (Candidate c: candidates) {
-    			if(!contReports.containsKey(c.sender))
-    				contReports.put(c.sender, new HashMap<>());
-    			HashMap<Transaction, Integer> m = (HashMap<Transaction, Integer>) contReports.get(c.sender);
-    			if(!m.containsKey(c.tx))
-    				m.put(c.tx, 1);
-    			else
-    				m.put(c.tx, m.get(c.tx)+1);
-    			if(!curReports.containsKey(c.sender))
-    				curReports.put(c.sender, new HashSet<>());
-    			curReports.get(c.sender).add(c.tx);
-    			
-    		}
-    		// remove transaction from the contReports if not reported in this round
-    		for(Iterator<Entry<Integer, Map<Transaction, Integer>>> senderIt =  contReports.entrySet().iterator();senderIt.hasNext();) {
-    			Entry<Integer, Map<Transaction, Integer>> senderEntry = senderIt.next();
-    			Integer sender  = senderEntry.getKey();
-    			if(!curReports.containsKey(sender)) 
-    				senderIt.remove();
-    			else
-    				for(Iterator<Entry<Transaction, Integer>> it = senderEntry.getValue().entrySet().iterator();it.hasNext();) {
-    					Transaction t = it.next().getKey();
-    					if(!curReports.get(sender).contains(t))
-    						it.remove();
-    				}
-    		}
-    		// accept the transactions which are reported continuously for more than a specific rounds
-    		for(Integer sender: contReports.keySet()) {
-    			for(Transaction t: contReports.get(sender).keySet()) {
-    				if(contReports.get(sender).get(t)>= nRounds*contFactor) {
-    					if(!txnSenders.containsKey(t) ) {
-    						txnSenders.put(t, new HashSet<>(Arrays.asList(sender)));
-    					}else {
-    						txnSenders.get(t).add(sender);
+    		if(doneRounds ==1) {
+    			for(Candidate c: candidates) {
+    				if(!lastTransmitted.containsKey(c.sender))
+    					lastTransmitted.put(c.sender, new HashSet<>());
+    				lastTransmitted.get(c.sender).add(c.tx);
+    			}
+    		}else if(doneRounds <=  nHold){
+    			Map<Integer, Set<Transaction>> txns = groupBySender(candidates);
+    			for(Integer sender: txns.keySet()) {
+    				if(! txns.get(sender).equals(lastTransmitted.get(sender)))
+    					malNodes.add(sender);
+    			}
+    			// if this is the last Hold Round, add all the transactions from honest nodes to received list
+    			if(doneRounds +1 > nHold) {
+    				for(Integer sender : lastTransmitted.keySet()) {
+    					for(Transaction t: lastTransmitted.get(sender)) {
+    						receivedTxns.add(t);
     					}
-    					receivedTxns.add(t);
     				}
     			}
+    		}else {
+    			for(Candidate c: candidates) {
+    				if(!malNodes.contains(c.sender))
+    					receivedTxns.add(c.tx);
+    			}
     		}
-			
-    		
+    }
+    private Map<Integer, Set<Transaction>> groupBySender(Set<Candidate> candidates){
+    		Map<Integer, Set<Transaction>> txns = new HashMap<>();
+    		for(Candidate c: candidates) {
+    			if(!txns.containsKey(c.sender))
+    				txns.put(c.sender, new HashSet<>());
+    			txns.get(c.sender).add(c.tx);
+    		}
+    		return txns;
     }
 }
